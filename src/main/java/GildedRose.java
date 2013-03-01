f84f75ae6d46491946d0e13e5dd4beaceb4e6ebc
Stefan Glase <stefan.glase@googlemail.com>
2013-03-01 21:19:32 +0100

    extract method: create item list

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -6,6 +6,12 @@ public class GildedRose {
     private static List<Item> items = null;
 
     public static void main(String[] args) {
+        createItemList();
+        printItemList(0);
+        updateQuality();
+    }
+
+    private static void createItemList() {
         items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
@@ -13,9 +19,6 @@ public class GildedRose {
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
-
-        printItemList(0);
-        updateQuality();
     }
 
     public static void printItemList(int days) {
