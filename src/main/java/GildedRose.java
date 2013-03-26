c0f1c38f651678b3ccb754695ed086fc423046a0
Stefan Glase <stefan.glase@googlemail.com>
2013-03-26 19:24:07 +0100

    text based testing introduced

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +8,12 @@ public class GildedRose {
     private static List<Item> items = null;
 
     public static void main(String[] args) {
-
         System.out.println("OMGHAI!");
+        initItems();
+        updateQuality();
+    }
 
+    public static void initItems() {
         items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
@@ -17,8 +21,12 @@ public class GildedRose {
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
+    }
 
-        updateQuality();
+    public static void printItems(PrintStream printStream) {
+        for (Item item : items) {
+            printStream.println(String.format("%45s %5s %5s", item.getName(), item.getSellIn(), item.getQuality()));
+        }
     }
 
     public static void updateQuality() {
